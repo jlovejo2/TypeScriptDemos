2f48e9bd307121182fdcfc715019c6c270770ce7
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-30 06:37:17 -0500

    added balance property to checking account class

--- examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
+++ examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
@@ -1,21 +1,4 @@
 ﻿export class CheckingAccount {
-    private _balance = 0; // field
-
-    constructor(public title: string) { }
-
-    get balance() { // get block
-        return this._balance;
-    }
-
-    set balance(val: number) { // set block
-        this._balance = val;
-    }
-
-    deposit(amount: number) { // function
-        this.balance += amount;
-    }
-
-    withdrawal(amount: number) { // function
-        this.balance -= amount;  
-    }
+    balance: number;
 }
+
