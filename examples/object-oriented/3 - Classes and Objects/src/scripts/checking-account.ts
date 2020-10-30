c711e08d30b50ab687a618fe8fcb0278d7eadba5
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-30 06:43:34 -0500

    added balance field, deposit and withdrawal functions to checking account class

--- examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
+++ examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
@@ -1,4 +1,20 @@
 ﻿export class CheckingAccount {
-    balance: number;
+    private _balance: number = 0;
+
+    get balance() {
+        return this._balance
+    }
+
+    set balance(value:number) {
+        this._balance = value;
+    }
+
+    deposit(amount: number) {
+        this._balance += amount;
+    }
+
+    withdrawal(amount: number) {
+        this.balance -= amount;
+    }
 }
 
